d54248c38a0d005de5e144243f94ca510d425f4d
Varreba <varreab@gmail.com>
2015-02-17 08:47:51 +0100

    V 0.1
    First commit of the project

---
+++ src/es/ats/codington/festival/domain/prueba1.java
@@ -0,0 +1,26 @@
+package es.ats.codington.festival.domain;
+
+public class prueba1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+System.out.println();
+
+		System.out.println("prueba");
+		System.out.println("javier");
+		
+		
+
+
+	}
+
+	public void metodoDummy1(int dummy){
+		
+	}
+	
+	
+	
+}
